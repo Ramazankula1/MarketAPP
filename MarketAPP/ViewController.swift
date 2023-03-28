ce2a5e9ec5c24b639dc836a766ac06c84e22f289
Ramazan Kula <ramazan.kula@live.de>
2023-03-28 22:29:43 +0200

    Programm fast fertig

--- MarketAPP/ViewController.swift
+++ MarketAPP/ViewController.swift
@@ -8,11 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var Artikelpreis: UITextField!
+    @IBOutlet weak var Endpreis: UILabel!
     
+    var Mwst: Double = 1.07
+    var Const: Double = 1.5
+    var artikel: Double?{
+        return Double(Artikelpreis.text!)
+    }
+    
+    @IBAction func MwStbestimmen(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            Mwst = 1.07
+         }
+        if sender.selectedSegmentIndex == 1 {
+            Mwst = 1.19
+         }
+     }
+    
+    @IBAction func Berechnen(_ sender: Any) {
+        if let artikel = self.artikel{
+            let total = artikel * Mwst * Const
+            Endpreis.text = String(format:"%.2f" ,total)
+        }
+    }
 }
-
